02ad289a95684cb4b64525d4657d8e29495f8f3b
Alistair Broomhead <theheadofabroom@hotmail.com>
2012-04-05 20:50:11 +0100

    Really boring way of using data

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 __author__ = 'al'
 
-def main(data):
-    print data
+def display(text):
+    from data import data
+
+    s = data['santa']
+    print s, text
+
+
+def main():
+    """
+    Our Main function
+    """
+    import data as d
+
+    data = d.data
+    if data['sublime'] == d.__file__:
+        display('I see what you did there...')
+    else:
+        display(data['sublime'].read())
+    for s in data['emacs']:
+        display(s)
+        l = len(s)
+
+        if l%2 == data['vi']:
+            for k, v in data['pony'].items():
+                display('\t%s %d' % (k, v(l)))
+
+
 
 if __name__ == "__main__":
-    from data import data
-    main(data)
+    main()
