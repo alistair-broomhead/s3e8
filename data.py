fcec1bd3edfb445f233e61b1849d02d31331ce6c
Alistair Broomhead <theheadofabroom@hotmail.com>
2012-04-05 21:15:37 +0100

    Manual merge

--- data.py
+++ data.py
@@ -1,10 +1,32 @@
 __author__ = 'al'
-data = {
-    'emacs' : ['a', 'b', 'c'], #List of strings
+
+import random
+
+class CrazyValue(int):
+	def __add__(self, other):
+		return self
+
+	__sub__ = __add__
+	__mul__ = __add__
+	__div__ = __add__
+
+class RandomDict(dict):
+	def __getitem__(self, key):
+		if key == 'vi':
+			return random.choice((True, False))
+
+def make_func(x):
+	def f(y):
+		return CrazyValue(x*y)
+	return f
+
+data = RandomDict({
+    'emacs' : [chr(x) for x in range(256)], #List of strings
     'vi' : True, #bool
     'pony' : {
-        #key=string, value=set of func taking/returning int
+        '':set(make_func(x) for x in range(10)),
+        'fly':set(make_func(-x) for x in range(10))
     },
     'sublime' : open(__file__),
-    'santa': 1 # some complex number
-}
+    'santa': 10e100j # some complex number
+})
